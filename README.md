6ed25ed9a31982bbd41f65e6e28fbf8fe2b244c7
Jay Lansiquot <jay@god.local>
2018-08-22 21:51:47 -0400

    change in direction slightly

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Tweaked-Transmission
 
 Eventually this will make using Transmission easier. The main thing of importance is allowing users to set a time in which to start a torrent using cronjobs within this tool.
+
+
+# Current requriements;
+cronjob
+
+# Setup
+Must first create a cronjob via crontab -e, that points to a file named torrents.sh, the script will append to torrents.sh.
+
--- get.py
+++ get.py
@@ -11,8 +11,6 @@ import argparse
 #wiLL run tranmssion on whatever host it is active on so will use this tool for lyr most likely
 
 
-
-
 parser = argparse.ArgumentParser()
 
 
@@ -21,6 +19,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--pause", default=0, help='Pauses all torrents')
 parser.add_argument("-a", "--add", help="Adds and starts the torrent")
 parser.add_argument("-r", "--remove", help="Remove all torrents")
+parser.add_argument("-t", "--time", help="Appends a cronjob of the command")
 
 
 args = parser.parse_args()
@@ -29,12 +28,24 @@ def transmission():
 	if args.pause:
 		os.system('transmission-remote -t all -S')
 	if args.add:
-		#torrentLink = input('Please enter the torrent link \n')
-		p2 = subprocess.call(["transmission-remote","-a", args.add])
-	if args.remove:
+		subprocess.call(["transmission-remote","-a", args.add])
+	if args.remove: #not working atm
 		os.system("transmission-remote -n 'transmission:transmission' -r")
+	if args.time:
+		with open ("/home/jay/crons/torrents.sh", "a") as myfile:
+			myfile.write('python3 /home/jay/python3/get.py -t {}\n'.format(args.time))
 
 
 if __name__ == '__main__':
 	transmission()
 
+
+#Has to add itself to crontab -e to the bottom of the file
+#OR
+#Overwrites a .sh file with the command output and then just have a cronjob just run that file always
+#	- Con would you be that if you don't clear/delete the .sh file cron will run that will keep running that .sh 
+
+#There will be a preset cronjob that points to a file, so we'll just append to that file
+#The torrent will start around 2 AM, brother should be offline by then.
+
+#Will have to stop the rsync cronjob on lord from running the option that checks and deletes files if it doesn't match what lord.
